bbd37c9bb56e8ee8859494579bc0465e70cc1275
Tofek Khan <tofekkhan@Tofeks-MacBook-Pro.local>
2023-12-28 22:50:46 -0600

    Initial commit

---
+++ src/main/java/com/tofek/example/spring/demo/SpringExampleDemoPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.tofek.example.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExampleDemoPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleDemoPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tofek/example/spring/demo/SpringExampleDemoPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tofek.example.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampleDemoPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
